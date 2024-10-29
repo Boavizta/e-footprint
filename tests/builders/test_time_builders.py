66d7df7676cb8e7cc8c6ee873a7228d9832706e5
julien.houbart_ext <julien.houbart_ext@sonepar.com>
2024-10-29 17:48:51 +0100

    [UPDATE] test Time builder frequency

--- tests/builders/test_time_builders.py
+++ tests/builders/test_time_builders.py
@@ -1,6 +1,7 @@
 import unittest
 from datetime import datetime, timedelta
 
+import pandas as pd
 import pint
 
 from efootprint.abstract_modeling_classes.source_objects import SourceHourlyValues
@@ -73,56 +74,112 @@ class TestTimeBuilders(unittest.TestCase):
         self.assertEqual(len(result.value), nb_of_hours)
         self.assertEqual(result.value['value'].pint.units, u.dimensionless)
 
-    def test_create_hourly_usage_from_frequency_case_1(self):
+
+    def test_create_hourly_usage_from_frequency_case_daily(self):
         input_volume = 100
+        frequency = "daily"
+        active_days = None
+        launch_hours = [9]
         start_date = datetime.strptime("2024-01-01", "%Y-%m-%d")
-        pint_unit = pint.Unit(u.dimensionless)
-        type_frequency = "daily"
-        duration = 14 * u.day
-        only_word_days = True
-        time_list = [9, 11]
-        result = create_hourly_usage_from_frequency(input_volume, start_date, pint_unit, type_frequency, duration,
-                                                    only_word_days, time_list)
+        end_date = datetime.strptime("2024-01-07 23:00", "%Y-%m-%d %H:%M")
+        duration = (end_date - start_date).days * u.day + (end_date - start_date).seconds * u.seconds
+
+        result = create_hourly_usage_from_frequency(
+            input_volume, duration, frequency, active_days, launch_hours, start_date
+        )
+
+        expected_index_populated = [9, 33, 57, 81, 105, 129, 153]
+
+        expected_max_period = pd.Period("2024-01-07 23:00", freq='h')
+
+        self.assertTrue(isinstance(result, SourceHourlyValues))
+        self.assertEqual(result.unit, u.dimensionless)
+        for index in range(0, len(result.value)):
+            if index in expected_index_populated:
+                self.assertEqual(result.value['value'].iloc[index], input_volume)
+            else:
+                self.assertEqual(result.value['value'].iloc[index], 0)
+        self.assertEqual(max(result.value.index), expected_max_period)
+
+
+    def test_create_hourly_usage_from_frequency_case_weekly(self):
+        input_volume = 100
+        frequency = "weekly"
+        active_days = [0,1 , 2, 3, 4]
+        launch_hours = [9, 11]
+        start_date = datetime.strptime("2024-01-01", "%Y-%m-%d")
+        end_date = datetime.strptime("2024-01-14 23:00", "%Y-%m-%d %H:%M")
+        duration = (end_date - start_date).days * u.day + (end_date - start_date).seconds * u.seconds
+
+        result = create_hourly_usage_from_frequency(
+            input_volume, duration, frequency, active_days, launch_hours, start_date
+        )
 
         expected_index_populated = [9, 11, 33, 35, 57, 59, 81, 83, 105, 107, 177, 179, 201, 203, 225, 227,
                             249, 251, 273, 275]
 
-        expected_max_date = datetime.strptime("2024-01-14 23:00", "%Y-%m-%d %H:%M")
+        expected_max_period = pd.Period("2024-01-14 23:00", freq='h')
 
         self.assertTrue(isinstance(result, SourceHourlyValues))
-        self.assertEqual(result.unit, pint_unit)
+        self.assertEqual(result.unit, u.dimensionless)
         for index in range(0, len(result.value)):
             if index in expected_index_populated:
                 self.assertEqual(result.value['value'].iloc[index], input_volume)
             else:
                 self.assertEqual(result.value['value'].iloc[index], 0)
-        self.assertEqual(max(result.value.index), expected_max_date)
+        self.assertEqual(max(result.value.index), expected_max_period)
 
 
 
-    def test_create_hourly_usage_from_frequency_case_3(self):
+    def test_create_hourly_usage_from_frequency_case_monthly(self):
         input_volume = 100
+        frequency = "monthly"
+        active_days = [1, 6, 11, 16, 21, 26]
+        launch_hours = [9, 11]
         start_date = datetime.strptime("2024-01-01", "%Y-%m-%d")
-        pint_unit = pint.Unit(u.dimensionless)
-        type_frequency = "monthly"
-        duration = 2 * u.month
-        only_word_days = True
-        time_list = [1, 6, 11, 16, 21, 26]
-        launch_hours_list = [9, 11]
-        result = create_hourly_usage_from_frequency(input_volume, start_date, pint_unit, type_frequency, duration,
-                                                    only_word_days, time_list,  launch_hours_list)
+        end_date = datetime.strptime("2024-02-29 23:00", "%Y-%m-%d %H:%M")
+        duration = (end_date - start_date).days * u.day + (end_date - start_date).seconds * u.seconds
+
+        result = create_hourly_usage_from_frequency(
+            input_volume, duration, frequency, active_days, launch_hours, start_date
+        )
 
         expected_index_populated = [9, 11, 129, 131, 249, 251, 369, 371, 489, 491, 609, 611 ,753, 755 ,873, 875 ,993,
                                     995 ,1113, 1115, 1233, 1235 ,1353, 1355]
 
-        expected_max_date = datetime.strptime("2024-02-29 23:00", "%Y-%m-%d %H:%M")
+        expected_max_period = pd.Period("2024-02-29 23:00", freq='h')
 
         self.assertTrue(isinstance(result, SourceHourlyValues))
-        self.assertEqual(result.unit, pint_unit)
-        self.assertEqual(max(result.value.index), expected_max_date)
+        self.assertEqual(result.unit,u.dimensionless)
+        self.assertEqual(max(result.value.index), expected_max_period)
         for index in range(0, len(result.value)):
             if index in expected_index_populated:
                 self.assertEqual(result.value['value'].iloc[index], input_volume)
             else:
                 self.assertEqual(result.value['value'].iloc[index], 0)
-        self.assertEqual(max(result.value.index), expected_max_date)
+
+    def test_create_hourly_usage_from_frequency_case_yearly(self):
+        input_volume = 100
+        frequency = "yearly"
+        active_days = [1]
+        launch_hours = [12]
+        start_date = datetime.strptime("2024-01-01", "%Y-%m-%d")
+        end_date = datetime.strptime("2026-12-31 23:00", "%Y-%m-%d %H:%M")
+        duration = (end_date - start_date).days * u.day + (end_date - start_date).seconds * u.seconds
+
+        result = create_hourly_usage_from_frequency(
+            input_volume, duration, frequency, active_days, launch_hours, start_date
+        )
+
+        expected_index_populated = [12, 8796, 17556]
+
+        expected_max_period = pd.Period("2026-12-31 23:00", freq='h')
+
+        self.assertTrue(isinstance(result, SourceHourlyValues))
+        self.assertEqual(result.unit,u.dimensionless)
+        self.assertEqual(max(result.value.index), expected_max_period)
+        for index in range(0, len(result.value)):
+            if index in expected_index_populated:
+                self.assertEqual(result.value['value'].iloc[index], input_volume)
+            else:
+                self.assertEqual(result.value['value'].iloc[index], 0)
