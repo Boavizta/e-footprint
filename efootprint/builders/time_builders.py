4ddc6c2f595e4d16a5eed0ba4b01c6b191f54d78
Vincent Villet <vincent.villet@gmail.com>
2024-11-04 14:58:34 +0100

    [REFACTO] time builders and their tests

--- efootprint/builders/time_builders.py
+++ efootprint/builders/time_builders.py
@@ -4,16 +4,16 @@ from typing import List
 import numpy as np
 import pandas as pd
 import pint
-from dateutil.relativedelta import relativedelta
 
 from efootprint.constants.units import u
 from efootprint.abstract_modeling_classes.source_objects import SourceHourlyValues
 
 
 def create_random_hourly_usage_df(
-        nb_days: int = 1, min_val: int = 1, max_val: int = 10,
+        timespan: pint.Quantity = 1 * u.day, min_val: int = 1, max_val: int = 10,
         start_date: datetime = datetime.strptime("2025-01-01", "%Y-%m-%d"),
         pint_unit: pint.Unit = u.dimensionless):
+    nb_days = timespan.to(u.day).magnitude
     end_date = start_date + timedelta(days=nb_days)
     period_index = pd.period_range(start=start_date, end=end_date, freq='h')
 
@@ -35,9 +35,10 @@ def create_hourly_usage_df_from_list(
 
 
 def linear_growth_hourly_values(
-        nb_of_hours: int, start_value: int, end_value: int,
+        timespan: pint.Quantity, start_value: int, end_value: int,
         start_date: datetime = datetime.strptime("2025-01-01", "%Y-%m-%d"),
         pint_unit: pint.Unit = u.dimensionless):
+    nb_of_hours = int(timespan.to(u.hour).magnitude)
     linear_growth = np.linspace(start_value, end_value, nb_of_hours)
 
     df = create_hourly_usage_df_from_list(linear_growth, start_date, pint_unit)
@@ -46,9 +47,10 @@ def linear_growth_hourly_values(
 
 
 def sinusoidal_fluct_hourly_values(
-        nb_of_hours: int, sin_fluct_amplitude: int, sin_fluct_period_in_hours: int,
+        timespan: pint.Quantity, sin_fluct_amplitude: int, sin_fluct_period_in_hours: int,
         start_date: datetime = datetime.strptime("2025-01-01", "%Y-%m-%d"),
         pint_unit: pint.Unit = u.dimensionless):
+    nb_of_hours = int(timespan.to(u.hour).magnitude)
     time = np.arange(nb_of_hours)
     sinusoidal_fluctuation = sin_fluct_amplitude * np.sin(2 * np.pi * time / sin_fluct_period_in_hours)
 
@@ -57,11 +59,13 @@ def sinusoidal_fluct_hourly_values(
     return SourceHourlyValues(df)
 
 
-def daily_fluct_hourly_values(nb_of_hours: int, fluct_scale: float, hour_of_day_for_min_value: int = 4,
-                              start_date: datetime = datetime.strptime("2025-01-01", "%Y-%m-%d"),
-                              pint_unit: pint.Unit = u.dimensionless):
+def daily_fluct_hourly_values(
+        timespan: pint.Quantity, fluct_scale: float, hour_of_day_for_min_value: int = 4,
+        start_date: datetime = datetime.strptime("2025-01-01", "%Y-%m-%d"),
+        pint_unit: pint.Unit = u.dimensionless):
     assert fluct_scale > 0
     assert fluct_scale <= 1
+    nb_of_hours = int(timespan.to(u.hour).magnitude)
     time = np.arange(nb_of_hours)
     hour_of_day = [(start_date.hour + x) % 24 for x in time]
 
@@ -81,30 +85,9 @@ def daily_fluct_hourly_values(nb_of_hours: int, fluct_scale: float, hour_of_day_
     return SourceHourlyValues(df)
 
 
-def create_hourly_usage_from_volume_and_list_of_hour(input_volume: float, start_date: datetime, pint_unit: pint.Unit,
-                                                     list_hour: List[int], duration: int):
-    if start_date is None:
-        start_date = datetime.strptime("2025-01-01", "%Y-%m-%d")
-    if pint_unit is None:
-        pint_unit = pint.Unit(u.dimensionless)
-    if duration is None:
-        duration = 365*24
-
-    volume_per_hour = round(input_volume / len(set(list_hour)),0)
-
-    date_range = pd.period_range(start=start_date, periods=duration, freq='h')
-    df = pd.DataFrame(0, index=date_range, columns=['value'], dtype=f"pint[{str(pint_unit)}]")
-
-    for index_hour in range(0, duration):
-        current_hour = start_date + timedelta(hours=index_hour)
-        if current_hour.hour in list_hour:
-            df.at[current_hour, 'value'] = volume_per_hour
-    return SourceHourlyValues(df, label="Hourly usage")
-
-
 def create_hourly_usage_from_frequency(
-        input_volume: float, duration: pint.Quantity, frequency: str, active_days: list = None,
-        launch_hours: list = None, start_date: datetime = datetime.strptime("2025-01-01", "%Y-%m-%d"),
+        timespan: pint.Quantity, input_volume: float, frequency: str, active_days: list = None,
+        hours: list = None, start_date: datetime = datetime.strptime("2025-01-01", "%Y-%m-%d"),
         pint_unit: pint.Unit = u.dimensionless):
     if frequency not in ['daily', 'weekly', 'monthly', 'yearly']:
         raise ValueError(f"frequency must be one of 'daily', 'weekly', 'monthly', or 'yearly', got {frequency}.")
@@ -112,7 +95,7 @@ def create_hourly_usage_from_frequency(
     if frequency == 'daily' and active_days is not None:
         raise ValueError(f"active_days must be None for daily frequency, got {active_days}.")
 
-    end_date = start_date + timedelta(days=duration.to(u.day).magnitude)
+    end_date = start_date + timedelta(days=timespan.to(u.day).magnitude)
 
     if active_days is None:
         if frequency == "weekly":
@@ -120,8 +103,8 @@ def create_hourly_usage_from_frequency(
         else:
             active_days = [1]  # default to first day of month or first day of year
 
-    if launch_hours is None:
-        launch_hours = [0]  # default to midnight
+    if hours is None:
+        hours = [0]  # default to midnight
 
     period_index = pd.period_range(start=start_date, end=end_date, freq='h')
     values = np.full(shape=len(period_index), fill_value=0)
@@ -132,18 +115,29 @@ def create_hourly_usage_from_frequency(
         day_of_month = period.day  # Day of the month, 1 to 31
         day_of_year = period.day_of_year  # Day of the year, 1 to 365/366
         if frequency == 'daily':
-            if hour_of_day in launch_hours:
+            if hour_of_day in hours:
                 values[i] = input_volume
         elif frequency == 'weekly':
-            if day_of_week in active_days and hour_of_day in launch_hours:
+            if day_of_week in active_days and hour_of_day in hours:
                 values[i] = input_volume
         elif frequency == 'monthly':
-            if day_of_month in active_days and hour_of_day in launch_hours:
+            if day_of_month in active_days and hour_of_day in hours:
                 values[i] = input_volume
         elif frequency == 'yearly':
-            if day_of_year in active_days and hour_of_day in launch_hours:
+            if day_of_year in active_days and hour_of_day in hours:
                 values[i] = input_volume
 
     df = pd.DataFrame(values, index=period_index, columns=['value'], dtype=f"pint[{str(pint_unit)}]")
 
     return SourceHourlyValues(df, label="Hourly usage")
+
+
+def create_hourly_usage_from_daily_volume_and_list_of_hours(
+        timespan: pint.Quantity, daily_volume: float, hours: List[int],
+        start_date: datetime = datetime.strptime("2025-01-01", "%Y-%m-%d"),
+        pint_unit: pint.Unit = u.dimensionless):
+    volume_per_hour = daily_volume / len(hours)
+
+    return create_hourly_usage_from_frequency(
+        timespan, volume_per_hour, frequency='daily', active_days=None, hours=hours,
+        start_date=start_date, pint_unit=pint_unit)
--- tests/builders/test_time_builders.py
+++ tests/builders/test_time_builders.py
@@ -2,23 +2,24 @@ import unittest
 from datetime import datetime, timedelta
 
 import pandas as pd
-import pint
 
 from efootprint.abstract_modeling_classes.source_objects import SourceHourlyValues
 from efootprint.builders.time_builders import (
     create_random_hourly_usage_df, create_hourly_usage_df_from_list, linear_growth_hourly_values,
-    sinusoidal_fluct_hourly_values, daily_fluct_hourly_values, create_hourly_usage_from_frequency)
+    sinusoidal_fluct_hourly_values, daily_fluct_hourly_values, create_hourly_usage_from_frequency,
+    create_hourly_usage_from_daily_volume_and_list_of_hours)
 from efootprint.constants.units import u
 
 
 class TestTimeBuilders(unittest.TestCase):
     def test_create_random_hourly_usage_df(self):
         nb_days = 2
+        timespan = nb_days * u.day
         min_val = 1
         max_val = 27
         start_date = datetime.strptime("2025-07-14", "%Y-%m-%d")
         pint_unit = u.dimensionless
-        df = create_random_hourly_usage_df(nb_days, min_val, max_val, start_date, pint_unit)
+        df = create_random_hourly_usage_df(timespan, min_val, max_val, start_date, pint_unit)
 
         self.assertEqual(start_date, datetime.strptime(df.index.min().strftime("%Y-%m-%d"), "%Y-%m-%d"))
         self.assertEqual(nb_days * 24 + 1, len(df))
@@ -40,23 +41,25 @@ class TestTimeBuilders(unittest.TestCase):
 
     def test_linear_growth_hourly_values(self):
         nb_of_hours = 5
+        timespan = nb_of_hours * u.hour
         start_value = 10
         end_value = 20
-        result = linear_growth_hourly_values(nb_of_hours, start_value, end_value)
+        result = linear_growth_hourly_values(timespan, start_value, end_value)
 
         self.assertTrue(isinstance(result, SourceHourlyValues))
         self.assertEqual(len(result.value), nb_of_hours)
-        self.assertEqual(result.value['value'][0], 10)
-        self.assertEqual(result.value['value'][-1], 20)
+        self.assertEqual(result.value['value'].iloc[0], 10)
+        self.assertEqual(result.value['value'].iloc[-1], 20)
 
     def test_sinusoidal_fluct_hourly_values(self):
-        nb_of_hours = 24 * 100
+        nb_of_days = 100
+        timespan = nb_of_days * u.day
         amplitude = 10
         period_in_hours = 24 * 10
-        result = sinusoidal_fluct_hourly_values(nb_of_hours, amplitude, period_in_hours)
+        result = sinusoidal_fluct_hourly_values(timespan, amplitude, period_in_hours)
 
         self.assertTrue(isinstance(result, SourceHourlyValues))
-        self.assertEqual(len(result.value), nb_of_hours)
+        self.assertEqual(len(result.value), nb_of_days * 24)
         self.assertLessEqual(result.value["value"].max().magnitude, amplitude)
         self.assertGreaterEqual(result.value["value"].min().magnitude, -amplitude)
         self.assertGreaterEqual(result.value["value"].max().magnitude, amplitude * 0.9)
@@ -64,16 +67,15 @@ class TestTimeBuilders(unittest.TestCase):
         self.assertEqual(result.value['value'].pint.units, u.dimensionless)
 
     def test_daily_fluct_hourly_values(self):
-        nb_of_hours = 24
+        timespan = 24 * u.hour
         fluct_scale = 0.5
         hour_of_min = 4
-        result = daily_fluct_hourly_values(nb_of_hours, fluct_scale, hour_of_min)
+        result = daily_fluct_hourly_values(timespan, fluct_scale, hour_of_min)
 
         self.assertTrue(isinstance(result, SourceHourlyValues))
         self.assertEqual(1-fluct_scale, result.value["value"].iloc[hour_of_min].magnitude)
-        self.assertEqual(len(result.value), nb_of_hours)
+        self.assertEqual(len(result.value), 24)
         self.assertEqual(result.value['value'].pint.units, u.dimensionless)
-
 
     def test_create_hourly_usage_from_frequency_case_daily(self):
         input_volume = 100
@@ -85,7 +87,7 @@ class TestTimeBuilders(unittest.TestCase):
         duration = (end_date - start_date).days * u.day + (end_date - start_date).seconds * u.seconds
 
         result = create_hourly_usage_from_frequency(
-            input_volume, duration, frequency, active_days, launch_hours, start_date
+            duration, input_volume, frequency, active_days, launch_hours, start_date
         )
 
         expected_index_populated = [9, 33, 57, 81, 105, 129, 153]
@@ -101,22 +103,45 @@ class TestTimeBuilders(unittest.TestCase):
                 self.assertEqual(result.value['value'].iloc[index], 0)
         self.assertEqual(max(result.value.index), expected_max_period)
 
+    def test_create_hourly_usage_from_daily_volume_and_list_of_hours(self):
+        daily_volume = 100
+        launch_hours = [9, 10]
+        start_date = datetime.strptime("2024-01-01", "%Y-%m-%d")
+        end_date = datetime.strptime("2024-01-07 23:00", "%Y-%m-%d %H:%M")
+        duration = (end_date - start_date).days * u.day + (end_date - start_date).seconds * u.seconds
+
+        result = create_hourly_usage_from_daily_volume_and_list_of_hours(
+            duration, daily_volume, launch_hours, start_date
+        )
+
+        expected_index_populated = [9, 10, 33, 34, 57, 58, 81, 82, 105, 106, 129, 130, 153, 154]
+
+        expected_max_period = pd.Period("2024-01-07 23:00", freq='h')
+
+        self.assertTrue(isinstance(result, SourceHourlyValues))
+        self.assertEqual(result.unit, u.dimensionless)
+        for index in range(0, len(result.value)):
+            if index in expected_index_populated:
+                self.assertEqual(result.value['value'].iloc[index], 50)
+            else:
+                self.assertEqual(result.value['value'].iloc[index], 0)
+        self.assertEqual(max(result.value.index), expected_max_period)
 
     def test_create_hourly_usage_from_frequency_case_weekly(self):
         input_volume = 100
         frequency = "weekly"
-        active_days = [0,1 , 2, 3, 4]
+        active_days = [0, 1, 2, 3, 4]
         launch_hours = [9, 11]
         start_date = datetime.strptime("2024-01-01", "%Y-%m-%d")
         end_date = datetime.strptime("2024-01-14 23:00", "%Y-%m-%d %H:%M")
         duration = (end_date - start_date).days * u.day + (end_date - start_date).seconds * u.seconds
 
         result = create_hourly_usage_from_frequency(
-            input_volume, duration, frequency, active_days, launch_hours, start_date
+            duration, input_volume, frequency, active_days, launch_hours, start_date
         )
 
-        expected_index_populated = [9, 11, 33, 35, 57, 59, 81, 83, 105, 107, 177, 179, 201, 203, 225, 227,
-                            249, 251, 273, 275]
+        expected_index_populated = [
+            9, 11, 33, 35, 57, 59, 81, 83, 105, 107, 177, 179, 201, 203, 225, 227, 249, 251, 273, 275]
 
         expected_max_period = pd.Period("2024-01-14 23:00", freq='h')
 
@@ -129,8 +154,6 @@ class TestTimeBuilders(unittest.TestCase):
                 self.assertEqual(result.value['value'].iloc[index], 0)
         self.assertEqual(max(result.value.index), expected_max_period)
 
-
-
     def test_create_hourly_usage_from_frequency_case_monthly(self):
         input_volume = 100
         frequency = "monthly"
@@ -141,11 +164,11 @@ class TestTimeBuilders(unittest.TestCase):
         duration = (end_date - start_date).days * u.day + (end_date - start_date).seconds * u.seconds
 
         result = create_hourly_usage_from_frequency(
-            input_volume, duration, frequency, active_days, launch_hours, start_date
+            duration, input_volume, frequency, active_days, launch_hours, start_date
         )
 
-        expected_index_populated = [9, 11, 129, 131, 249, 251, 369, 371, 489, 491, 609, 611 ,753, 755 ,873, 875 ,993,
-                                    995 ,1113, 1115, 1233, 1235 ,1353, 1355]
+        expected_index_populated = [9, 11, 129, 131, 249, 251, 369, 371, 489, 491, 609, 611, 753, 755, 873, 875, 993,
+                                    995, 1113, 1115, 1233, 1235, 1353, 1355]
 
         expected_max_period = pd.Period("2024-02-29 23:00", freq='h')
 
@@ -168,7 +191,7 @@ class TestTimeBuilders(unittest.TestCase):
         duration = (end_date - start_date).days * u.day + (end_date - start_date).seconds * u.seconds
 
         result = create_hourly_usage_from_frequency(
-            input_volume, duration, frequency, active_days, launch_hours, start_date
+            duration, input_volume, frequency, active_days, launch_hours, start_date
         )
 
         expected_index_populated = [12, 8796, 17556]
