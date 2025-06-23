b27467a612bd1577154d44bd46a67959eae5397b
Vincent Villet <vincent.villet@gmail.com>
2025-06-23 10:26:43 +0200

    [FIX] Disable Pint cache folder to avoid errors when using e-footprint in a web server. Unit registry instanciation now takes ~0.1s instead of 0.01s.

--- efootprint/constants/units.py
+++ efootprint/constants/units.py
@@ -5,7 +5,7 @@ from pint import UnitRegistry
 
 from efootprint.constants.files import CUSTOM_UNITS_PATH
 
-u = UnitRegistry(cache_folder=":auto:")
+u = UnitRegistry()
 u.load_definitions(CUSTOM_UNITS_PATH)
 u.default_locale = 'en_EN'
 pint.set_application_registry(u)
