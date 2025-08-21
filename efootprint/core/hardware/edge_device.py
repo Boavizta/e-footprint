86ce145fa28c5d2ce341d145876c122b3f301913
VincentVillet <vincent.villet@gmail.com>
2025-08-21 10:10:56 +0200

    [WIP2] EdgeDevice rework

--- efootprint/core/hardware/edge_device.py
+++ efootprint/core/hardware/edge_device.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional, TYPE_CHECKING
 
 from efootprint.abstract_modeling_classes.explainable_quantity import ExplainableQuantity
 from efootprint.abstract_modeling_classes.empty_explainable_object import EmptyExplainableObject
@@ -7,6 +7,12 @@ from efootprint.abstract_modeling_classes.source_objects import SourceValue
 from efootprint.constants.units import u
 from efootprint.core.hardware.storage import Storage
 from efootprint.core.hardware.infra_hardware import InfraHardware
+from efootprint.core.hardware.infra_hardware import InsufficientCapacityError
+
+if TYPE_CHECKING:
+    from efootprint.core.usage.edge_usage_journey import EdgeUsageJourney
+    from efootprint.core.usage.edge_usage_pattern import EdgeUsagePattern
+    from efootprint.abstract_modeling_classes.explainable_hourly_quantities import ExplainableHourlyQuantities
 
 
 class EdgeDevice(InfraHardware):
@@ -30,16 +36,13 @@ class EdgeDevice(InfraHardware):
                  server_utilization_rate: ExplainableQuantity, base_ram_consumption: ExplainableQuantity,
                  base_compute_consumption: ExplainableQuantity, storage: Storage):
         super().__init__(name, carbon_footprint_fabrication, power, lifespan)
-        
-        # Edge device specific attributes
-        self.hour_by_hour_compute_need = EmptyExplainableObject()
-        self.hour_by_hour_ram_need = EmptyExplainableObject()
+        del self.raw_nb_of_instances
+        del self.nb_of_instances # overwritten by nb_of_instances_property
         self.available_compute_per_instance = EmptyExplainableObject()
         self.available_ram_per_instance = EmptyExplainableObject()
-        self.occupied_ram_per_instance = EmptyExplainableObject()
-        self.occupied_compute_per_instance = EmptyExplainableObject()
-        
-        # Parameters
+        self.unitary_hourly_compute_need_over_full_timespan = EmptyExplainableObject()
+        self.unitary_hourly_ram_need_over_full_timespan = EmptyExplainableObject()
+
         self.idle_power = idle_power.set_label(f"Idle power of {self.name}")
         self.ram = ram.set_label(f"RAM of {self.name}")
         self.compute = compute.set_label(f"Compute of {self.name}")
@@ -52,18 +55,26 @@ class EdgeDevice(InfraHardware):
     @property
     def calculated_attributes(self):
         return super().calculated_attributes + [
-            "available_ram_per_instance", "available_compute_per_instance"
-            "hour_by_hour_ram_need", "hour_by_hour_compute_need",
+            "available_ram_per_instance", "available_compute_per_instance",
+            "unitary_hourly_ram_need_over_full_timespan", "unitary_hourly_compute_need_over_full_timespan",
+            "instances_fabrication_footprint", "instances_energy", "energy_footprint"
         ]
 
     @property
-    def edge_usage_journey(self):
+    def edge_usage_journey(self) -> Optional["EdgeUsageJourney"]:
         if self.modeling_obj_containers:
             if len(self.modeling_obj_containers) > 1:
                 raise PermissionError(
-                    f"EdgeDevice object can only be associated with one EdgeUsageJourney object but {self.name} is associated "
-                    f"with {[mod_obj.name for mod_obj in self.modeling_obj_containers]}")
+                    f"EdgeDevice object can only be associated with one EdgeUsageJourney object but {self.name} is "
+                    f"associated with {[mod_obj.name for mod_obj in self.modeling_obj_containers]}")
             return self.modeling_obj_containers[0]
+        else:
+            return None
+
+    @property
+    def edge_usage_pattern(self) -> Optional["EdgeUsagePattern"]:
+        if self.modeling_obj_containers:
+            return self.edge_usage_journey.edge_usage_pattern
         else:
             return None
 
@@ -75,72 +86,62 @@ class EdgeDevice(InfraHardware):
     def edge_processes(self) -> List:
         return self.edge_usage_journey.edge_processes
 
-    def update_hour_by_hour_ram_need(self):
-        hour_by_hour_ram_needs = EmptyExplainableObject()
-        for edge_process in self.edge_processes:
-            # EdgeProcess will have recurrent_ram_compute that defines weekly hourly consumption
-            hour_by_hour_ram_needs += edge_process.hourly_ram_consumption
-        
-        self.hour_by_hour_ram_need = hour_by_hour_ram_needs.to(u.GB).set_label(f"{self.name} hour by hour RAM need")
+    @property
+    def nb_of_instances(self) -> "ExplainableHourlyQuantities" | EmptyExplainableObject:
+        if self.edge_usage_pattern:
+            return self.edge_usage_pattern.nb_edge_usage_journeys_in_parallel
+        return EmptyExplainableObject()
 
-    def update_hour_by_hour_compute_need(self):
-        hour_by_hour_compute_needs = EmptyExplainableObject()
-        for edge_process in self.edge_processes:
-            # EdgeProcess will have recurrent_cpu_compute that defines weekly hourly consumption
-            hour_by_hour_compute_needs += edge_process.hourly_compute_consumption
-        
-        self.hour_by_hour_compute_need = hour_by_hour_compute_needs.to(u.cpu_core).set_label(f"{self.name} hour by hour compute need")
+    def update_raw_nb_of_instances(self):
+        pass
 
-    def validate_resource_consumption(self):
-        """
-        Validate that the total resource consumption of all edge processes
-        doesn't exceed the edge device capacity.
-        """
-        # Check each hour of the week
-        for hour in range(168):  # 168 hours in a week
-            total_cpu = sum(process.recurrent_cpu_compute[hour] for process in self.edge_processes)
-            total_ram = sum(process.recurrent_ram_compute[hour] for process in self.edge_processes)
-
-            # Check against available capacity (considering utilization rate and base consumption)
-            available_cpu = (
-                        self.edge_device.compute.value.magnitude * self.edge_device.server_utilization_rate.value.magnitude
-                        - self.edge_device.base_compute_consumption.value.magnitude)
-            available_ram = (self.edge_device.ram.value.to(
-                u.GB).magnitude * self.edge_device.server_utilization_rate.value.magnitude
-                             - self.edge_device.base_ram_consumption.value.to(u.GB).magnitude)
-
-            if total_cpu > available_cpu:
-                raise ValueError(
-                    f"Hour {hour}: Total CPU consumption ({total_cpu}) exceeds available capacity ({available_cpu}) "
-                    f"on {self.edge_device.name}")
-
-            if total_ram > available_ram:
-                raise ValueError(
-                    f"Hour {hour}: Total RAM consumption ({total_ram} GB) exceeds available capacity ({available_ram} GB) "
-                    f"on {self.edge_device.name}")
-
-    def update_occupied_ram_per_instance(self):
-        self.occupied_ram_per_instance = self.base_ram_consumption.set_label(f"Occupied RAM per {self.name} instance")
-
-    def update_occupied_compute_per_instance(self):
-        self.occupied_compute_per_instance = self.base_compute_consumption.set_label(f"Occupied compute per {self.name} instance")
+    def update_nb_of_instances(self):
+        pass
 
     def update_available_ram_per_instance(self):
-        available_ram_per_instance = (self.ram * self.server_utilization_rate - self.occupied_ram_per_instance)
+        available_ram_per_instance = (self.ram * self.server_utilization_rate - self.base_ram_consumption)
         
-        from efootprint.core.hardware.infra_hardware import InsufficientCapacityError
         if available_ram_per_instance.value < 0 * u.B:
             raise InsufficientCapacityError(
-                self, "RAM", self.ram * self.server_utilization_rate, self.occupied_ram_per_instance)
+                self, "RAM", self.ram * self.server_utilization_rate, self.base_ram_consumption)
 
-        self.available_ram_per_instance = available_ram_per_instance.set_label(f"Available RAM per {self.name} instance")
+        self.available_ram_per_instance = available_ram_per_instance.set_label(
+            f"Available RAM per {self.name} instance")
 
     def update_available_compute_per_instance(self):
-        available_compute_per_instance = (self.compute * self.server_utilization_rate - self.occupied_compute_per_instance)
-        
-        from efootprint.core.hardware.infra_hardware import InsufficientCapacityError
-        if available_compute_per_instance.value < 0:
-            raise InsufficientCapacityError(
-                self, "compute", self.compute * self.server_utilization_rate, self.occupied_compute_per_instance)
+        available_compute_per_instance = (self.compute * self.server_utilization_rate - self.base_compute_consumption)
 
-        self.available_compute_per_instance = available_compute_per_instance.set_label(f"Available compute per {self.name} instance")
+        if available_compute_per_instance.value < 0 * u.cpu_core:
+            raise InsufficientCapacityError(
+                self, "compute", self.compute * self.server_utilization_rate, self.base_compute_consumption)
+
+        self.available_compute_per_instance = available_compute_per_instance.set_label(
+            f"Available compute per {self.name} instance")
+
+    def update_unitary_hourly_ram_need_over_full_timespan(self):
+        unitary_hourly_ram_need_over_full_timespan = sum(
+            edge_process.unitary_hourly_ram_need_over_full_timespan for edge_process in self.edge_processes)
+
+        max_ram_need = self.unitary_hourly_ram_need_over_full_timespan.max().to(u.GB)
+        if max_ram_need.value > self.available_ram_per_instance.value:
+            raise InsufficientCapacityError(
+                self, "RAM", self.available_ram_per_instance, max_ram_need)
+
+        self.unitary_hourly_ram_need_over_full_timespan = unitary_hourly_ram_need_over_full_timespan.to(u.GB).set_label(
+            f"{self.name} hour by hour RAM need")
+
+    def update_unitary_hourly_compute_need_over_full_timespan(self):
+        unitary_hourly_compute_need_over_full_timespan = sum(
+            edge_process.unitary_hourly_compute_need_over_full_timespan for edge_process in self.edge_processes)
+
+        max_compute_need = self.unitary_hourly_compute_need_over_full_timespan.max().to(u.cpu_core)
+
+        if max_compute_need.value > self.available_compute_per_instance.value:
+            raise InsufficientCapacityError(
+                self, "compute", self.available_compute_per_instance, max_compute_need)
+        
+        self.unitary_hourly_compute_need_over_full_timespan = unitary_hourly_compute_need_over_full_timespan.to(
+            u.cpu_core).set_label(f"{self.name} hour by hour compute need")
+
+    def update_instances_energy(self):
+        pass
